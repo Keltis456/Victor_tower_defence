2e78c8a03d62a957288ea49922a3d7530652ea64
Keltis456 <svet_svb@mail.ru>
2018-06-28 19:47:18 +0300

    Init

---
+++ Assets/Scripts/HpBase.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class HpBase : MonoBehaviour {
+
+	public int HP = 150;
+	public  Text HPtext;
+
+	// Use this for initialization
+	void Update () 
+	{
+		HPtext.text = HP.ToString();
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("enemyBug"))
+		{
+			HP -= 10;
+			Destroy(other.gameObject);
+			Destroy(other.GetComponent<MoveToWayPoints>().hp);
+		}
+	}
+}
---
+++ Assets/Scripts/bulletTower.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletTower : MonoBehaviour {
+
+	public float Speed;
+	public Transform target;
+	public Tower twr;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+	if(target)
+	transform.position = Vector3.MoveTowards(transform.position,target.position,Time.deltaTime*Speed);
+	if(!target)
+	{
+		Destroy(gameObject);
+	}
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.transform == target)
+		{
+			target.GetComponent<MoveToWayPoints>().hp.GetComponent<HpBar>().Dmg(twr.dmg);
+			Destroy(gameObject);
+		}
+	}
+}
